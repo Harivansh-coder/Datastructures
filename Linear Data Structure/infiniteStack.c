6ccb08953cef9ee47ff2152d6e5b7d5d32490499
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2021-12-28 23:19:20 +0530

    few changes

--- Linear Data Structure/infiniteStack.c
+++ Linear Data Structure/infiniteStack.c
@@ -1,73 +1,90 @@
 // infinite only if your memory is infinite
-
 #include<stdio.h>
 #include <malloc.h>
 
 int size = 10;
 int count = 0;
 
-// this will add element in the stack
-void push(int *arr, int val){
-    if (count < size){
-        printf("normal");
-        *(arr + count) = val;
-        count++;
-    } else{
-        printf("hhh");
 
-        size *= 2*size;
-        int *arrNew = (int *)realloc(arr,size* sizeof(int));
-        for (int i = 0; i < count; i ++){
-            *(arrNew + i) = *(arr + i);
-        }
-
-        arr = arrNew;
-
+void printStack(const int *arr) {
+    for (int i = 0; i < count; i++) {
+        printf("%i ", *(arr + i));
     }
-
-
 
 }
 
+// this will add element in the stack
+void push(int *arr, int val) {
+    if (count < size) {
+        arr[count] = val;
+        count++;
+    } else {
+        size *= 2;
+        arr = (int *) realloc(arr, size * sizeof(int));
+
+        arr[count] = val;
+        count++;
+    }
+}
+
+
 // this will remove element from stack
-void pop(){
-    if (count == 0){
+void pop() {
+
+    if (count == 0) {
         printf("Array is empty");
         return;
     }
+    printf("\n");
     count -= 1;
 
 }
 
 
-void printStack(const int *arr){
-    for (int i = 0; i < count; i ++){
-        printf("%i ",*(arr + i));
-    }
-
-}
 
 
-int main(void)
-{
-    int *arr = (int *) malloc(size*(sizeof(int)));
+int main(void) {
+    int *arr = (int *) malloc(size * (sizeof(int)));
 
-
+    push(arr, 1);
+    push(arr, 2);
+    push(arr, 3);
+    push(arr, 4);
     push(arr, 5);
     push(arr, 6);
     push(arr, 7);
     push(arr, 8);
     push(arr, 9);
     push(arr, 10);
-    push(arr, 5);
-    push(arr, 6);
-    push(arr, 7);
-    push(arr, 8);
-    push(arr, 9);
-    push(arr, 10);
-
 
     printStack(arr);
+    printf("%i",size);
+
+    push(arr, 11);
+    push(arr, 12);
+    push(arr, 1);
+    push(arr, 2);
+    push(arr, 3);
+    push(arr, 4);
+    push(arr, 5);
+    push(arr, 6);
+    push(arr, 7);
+    push(arr, 8);
+    push(arr, 9);
+    push(arr, 10);
+    push(arr, 11);
+    push(arr, 12);
+
+    printStack(arr);
+    printf("%i",size);
+
+    pop();
+    pop();
+    pop();
+
+    printStack(arr);
+    printf("%i",size);
+
 
 
 
